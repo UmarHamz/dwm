33e86053b26bc3d7bd9f8d20600ba60cd3d01dde
Umar Khamzatkhanov <napi.adam.umar@gmail.com>
2026-01-14 17:55:22 +0300

    changed font size in config.h and added Telegram start up rule

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=11" };
+static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -31,6 +31,7 @@ static const Rule rules[] = {
 	{ "alacritty",     NULL,       NULL,       0,            0,           -1 },
 	{ "Hiddify",  	   NULL,       NULL,       1 << 7,       1,           -1 }, 
 	{ "zen",  	   NULL,       NULL,       1 << 1,       0,           -1 }, 
+	{ "TelegramDesktop",  	   NULL,       NULL,       1 << 8,       0,           -1 }, 
 };
 
 /* layout(s) */
