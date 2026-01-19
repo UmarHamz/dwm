76d54c3501ba9a3d124773b76b6de6fdb27d3a55
Umar Khamzatkhanov <napi.adam.umar@gmail.com>
2026-01-19 15:08:27 +0300

    changed font to JetBrainsMonoNerd, changed hotkey for closing window

--- config.h
+++ config.h
@@ -6,13 +6,13 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=11" };
-static const char dmenufont[]       = "monospace:size=11";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#444444";
+static const char col_cyan[]        = "#222222";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -32,6 +32,7 @@ static const Rule rules[] = {
 	{ "Hiddify",  	   NULL,       NULL,       1 << 7,       1,           -1 }, 
 	{ "zen",  	   NULL,       NULL,       1 << 1,       0,           -1 }, 
 	{ "TelegramDesktop",  	   NULL,       NULL,       1 << 8,       0,           -1 }, 
+	{ "Zathura",  	   NULL,       NULL,       1 << 2,       0,           -1 }, 
 };
 
 /* layout(s) */
@@ -85,7 +86,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,             		XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
