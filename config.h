d99d22d6b69969244315f685bb5b6b89c0a22a51
Umar Khamzatkhanov <napi.adam.umar@gmail.com>
2026-01-26 20:30:28 +0300

    applied vanitygaps patch

--- config.h
+++ config.h
@@ -4,6 +4,11 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11" };
@@ -33,6 +38,8 @@ static const Rule rules[] = {
 	{ "zen",  	   NULL,       NULL,       1 << 1,       0,           -1 }, 
 	{ "TelegramDesktop",  	   NULL,       NULL,       1 << 8,       0,           -1 }, 
 	{ "Zathura",  	   NULL,       NULL,       1 << 2,       0,           -1 }, 
+	{ "obs",  	   NULL,       NULL,       1 << 4,       0,           -1 }, 
+	{ "vlc",  	   NULL,       NULL,       1 << 3,       0,           -1 }, 
 };
 
 /* layout(s) */
@@ -41,11 +48,26 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
+	{ "H[]",      deck },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ NULL,       NULL },
 };
 
 /* key definitions */
@@ -73,10 +95,30 @@ static const char *dimmer[]   = { "brightnessctl", "set", "10%-", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
+ 	{ MODKEY|ShiftMask,                XK_h,      setcfact,       {.f = +0.25} },
+ 	{ MODKEY|ShiftMask,                XK_l,      setcfact,       {.f = -0.25} },
+ 	{ MODKEY|ShiftMask,                XK_o,      setcfact,       {.f =  0.00} },
+ 	{ MODKEY|ControlMask,              XK_u,      incrgaps,       {.i = +3 } },
+ 	{ MODKEY|ControlMask|ShiftMask,    XK_u,      incrgaps,       {.i = -3 } },
+ 	{ MODKEY|ControlMask,              XK_i,      incrigaps,      {.i = +3 } },
+ 	{ MODKEY|ControlMask|ShiftMask,    XK_i,      incrigaps,      {.i = -3 } },
+ 	{ MODKEY|ControlMask,              XK_o,      incrogaps,      {.i = +3 } },
+ 	{ MODKEY|ControlMask|ShiftMask,    XK_o,      incrogaps,      {.i = -3 } },
+ 	{ MODKEY|ControlMask,              XK_6,      incrihgaps,     {.i = +3 } },
+ 	{ MODKEY|ControlMask|ShiftMask,    XK_6,      incrihgaps,     {.i = -3 } },
+ 	{ MODKEY|ControlMask,              XK_7,      incrivgaps,     {.i = +3 } },
+ 	{ MODKEY|ControlMask|ShiftMask,    XK_7,      incrivgaps,     {.i = -3 } },
+ 	{ MODKEY|ControlMask,              XK_8,      incrohgaps,     {.i = +3 } },
+ 	{ MODKEY|ControlMask|ShiftMask,    XK_8,      incrohgaps,     {.i = -3 } },
+ 	{ MODKEY|ControlMask,              XK_9,      incrovgaps,     {.i = +3 } },
+ 	{ MODKEY|ControlMask|ShiftMask,    XK_9,      incrovgaps,     {.i = -3 } },
+ 	{ MODKEY|ControlMask,              XK_0,      togglegaps,     {0} },
+ 	{ MODKEY|ControlMask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+
 	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
  	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,	                      XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -86,7 +128,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,             		XK_q,      killclient,     {0} },
+	{ MODKEY,             		      XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -110,14 +152,14 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 
  	{ 0, 				XF86XK_AudioMute,          spawn, {.v = mute_vol } },
-        { 0, 				XF86XK_AudioLowerVolume,   spawn, {.v = down_vol } },
-        { 0, 				XF86XK_AudioRaiseVolume,   spawn, {.v = up_vol } },
+  { 0, 				XF86XK_AudioLowerVolume,   spawn, {.v = down_vol } },
+  { 0, 				XF86XK_AudioRaiseVolume,   spawn, {.v = up_vol } },
 
-        { 0, 				XF86XK_MonBrightnessDown,  spawn, {.v = dimmer } },
-        { 0, 				XF86XK_MonBrightnessUp,    spawn, {.v = brighter } },
-	
-    	{ 0,         			XK_Print, spawn, SHCMD("/home/marsho/bin/screenshot.sh") },
-       	{ ShiftMask, 			XK_Print, spawn, SHCMD("/home/marsho/bin/screenshotsel.sh") },
+  { 0, 				XF86XK_MonBrightnessDown,  spawn, {.v = dimmer } },
+  { 0, 				XF86XK_MonBrightnessUp,    spawn, {.v = brighter } },
+
+	{ 0,         			XK_Print, spawn, SHCMD("/home/marsho/bin/screenshot.sh") },
+ 	{ ShiftMask, 			XK_Print, spawn, SHCMD("/home/marsho/bin/screenshotsel.sh") },
 };
 
 /* button definitions */
